784ee42cad7bc850335f39ee4583e12fc2a0d098
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-01-26 11:19:11 -0300

    Criar URL amigável com htaccess

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -10,9 +10,22 @@ namespace App\adms\Controllers\Services;
  */
 class PageController
 {
-    // Método construtor: significa que toda vez que a classe for instanciada, o método será executado.
+    // Atributo que recebe a URL do .htaccess
+    private string $url;
+
+    // Método construtor: significa que toda vez que a classe for instanciada, o método será executado. Recebe a URL do .htaccess.
     public function __construct()
     {
-        echo 'PageController teste';
+        echo 'PageController <br>';
+
+        // Verifica se vem valor na variável url enviada pelo .htaccess
+        if (!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+            // Recebe a URL do .htaccess
+            $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+
+            echo "Acessar o endereço: " . $this->url . "<br>";
+        }else{
+            echo "Acessar a página inicial";
+        }
     }
 }
